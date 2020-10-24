c8b660fddd62b5affdba246f6a03269abec06165
Paulyne <paulynewambui2020@gmail.com>
2020-10-24 18:16:25 +0300

    add the sql database uri

--- config.py
+++ config.py
@@ -5,11 +5,14 @@ class Config:
     MOVIE_API_BASE_URL ='https://api.themoviedb.org/3/movie/{}?api_key=528a373c934d5f1f1ba09dfb35ea7602'
     MOVIE_API_KEY = os.environ.get('MOVIE_API_KEY')
     SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://wambui:neema@localhost/watchlist'
 
-
+    @staticmethod
+    def init_app(app):
+        pass
+    
 class ProdConfig(Config):
     pass
-
 
 class DevConfig(Config):
     DEBUG = True
